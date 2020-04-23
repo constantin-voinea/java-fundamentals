5430ceb6713d645845f2c4324828bab0630f842b
Constantin.Voinea <constantin.voinea@devpadawan.ro>
2020-04-23 23:14:59 +0300

    Add comment in supplier ex

--- course_8/src/functionalinterfaces/SupplierEx.java
+++ course_8/src/functionalinterfaces/SupplierEx.java
@@ -21,7 +21,7 @@ public class SupplierEx {
             }
         };
         Supplier<LocalDate> s2 = () -> LocalDate.now();
-        Supplier<LocalDate> s3 = LocalDate::now;
+        Supplier<LocalDate> s3 = LocalDate::now; // reference to a static method
 
         LocalDate d1 = s1.get();
         LocalDate d2 = s2.get();
@@ -31,11 +31,11 @@ public class SupplierEx {
         System.out.println(d3);
 
         Supplier<StringBuilder> sb1 = () -> new StringBuilder();
-        Supplier<StringBuilder> sb2 = StringBuilder::new;
+        Supplier<StringBuilder> sb2 = StringBuilder::new; // method reference to a constructor
         System.out.println(sb1.get().append("sb1"));
         System.out.println(sb2.get().append("sb2"));
 
-        Supplier<ArrayList<String>> sb3 = ArrayList::new;
+        Supplier<ArrayList<String>> sb3 = ArrayList::new; // method reference to a constructor
         ArrayList<String> ar1 = sb3.get();
         System.out.println(ar1);
 
