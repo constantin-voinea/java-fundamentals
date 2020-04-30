744476f2ad6cf1ab8f820a4cdecdfcf8287880c9
Constantin.Voinea <constantin.voinea@devpadawan.ro>
2020-04-30 17:46:12 +0300

    Add course_9 examples

---
+++ course_9/src/optional/Ex1.java
@@ -0,0 +1,43 @@
+package optional;
+
+import java.util.Optional;
+
+/**
+ * @author cvoinea
+ * <p>
+ * Optional class introduced in Java 8
+ * purpose of use: provide a type-level solution for representing optional values instead of null references
+ * Optional is meant to be used as a return type
+ * trying to use Optional as a field type is not recommended; in a serializable class it will result in a NotSerializableException
+ */
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        //creating Optional objects
+        // empty()
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty.isPresent()); // false
+
+        // of()
+        Optional<String> o1 = Optional.of("abcabc");
+        System.out.println(o1.isPresent()); //true
+        System.out.println(o1.isEmpty()); // starting with Java 11, opposite of isPresent()
+        System.out.println(o1); //Optional[abcabc]
+        // throws NPE if null is provided as argument
+//        System.out.println(Optional.of(null));
+
+        // ofNullable()
+        String s1 = null;
+        Optional<String> nullSafe = Optional.ofNullable(s1); // will create Optional.empty
+        System.out.println(nullSafe);
+
+        // ofNullable().orElse()
+        String s2 = Optional.ofNullable(s1).orElse(""); // returns default value provided in orElse() if first value is null
+
+        // ofNullable().orElseGet() -- similar with orElse(), but takes a Supplier as param
+        String s3 = Optional.ofNullable(s1).orElseGet(() -> "");
+        String s4 = Optional.ofNullable(s1).orElse("default");
+
+    }
+}
---
+++ course_9/src/streamsource/Ex3.java
@@ -0,0 +1,38 @@
+package streamsource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * @author cvoinea
+ * ways to create streams
+ */
+public class Ex3 {
+
+    public static void main(String[] args) {
+
+        // there is no CharStream interface in JDK, so use IntStream to represent a stream of chars
+        IntStream streamOfChars = "abcabcabc".chars();
+        streamOfChars.distinct().forEach(c -> System.out.println((char)c));
+
+        // stream of strings from Pattern class
+        Stream<String> streamOfString = Pattern.compile(",").splitAsStream("a,b,c,d,e,f,g,h");
+        streamOfString.forEach(System.out::println);
+
+        // stream of strings from Files class
+        Path path = Paths.get("course_9\\src\\streamsource\\Ex3.java");
+        Stream<String> streamOfLines = Stream.empty();
+        try {
+            streamOfLines = Files.lines(path);
+        } catch (IOException e) {
+            System.out.println("cannot read from file!");
+        }
+        streamOfLines.forEach(System.out::println);
+
+    }
+}
